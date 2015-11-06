8e78e956211bcd338e9b94e8be56f1c60835dbd7
Siavash Kavousi <siavash.kavousi.sk@gmail.com>
2015-11-06 16:10:48 +0330

    Changes shm.h to mman.h

--- ReaderWriterSample/main.cpp
+++ ReaderWriterSample/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <unistd.h>
-#include <sys/shm.h>
+#include <sys/mman.h>
 #include <semaphore.h>
 #include <fcntl.h>
 #include <sys/wait.h>
@@ -8,21 +8,26 @@
 using namespace std;
 
 int main() {
-    key_t shm_key;
-    int shm_id;
+    const char *name = "/value";
     sem_t *semaphore1, *semaphore2;
     pid_t pid;
-    int *val;
+    int *val = 0;
+    int fd;
 
-    shm_key = ftok("dev/null", 5);
-    cout << "shm key for val " << shm_key << endl;
-    if ((shm_id = shmget(shm_key, sizeof(int), 0644 | IPC_CREAT)) < 0) {
-        perror("shmget\n");
+    fd = shm_open(name, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
+        perror("failed to create shared memory");
+        exit(EXIT_FAILURE);
+    }
+    if ((ftruncate(fd, sizeof(val))) == -1) {
+        perror("failed to set size of the shared memory");
         exit(EXIT_FAILURE);
     }
 
-    val = (int *) shmat(shm_id, NULL, 0);
-    *val = 0;
+    val = (int *) mmap(NULL, sizeof(val), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (val == MAP_FAILED) {
+        perror("mapping failed");
+    }
     cout << "ptr " << *val << " is allocated in shared memory" << endl;
 
     // initialize semaphores for shared processes
@@ -75,24 +80,22 @@ int main() {
             val[0]++;
             cout << "writer " << getpid() << " writes " << val[0] << endl;
             sleep(1);
-            sem_post(semaphore1);
             cout << "writer " << getpid() << " releases lock 1" << endl;
-            sem_post(semaphore2);
+            sem_post(semaphore1);
             cout << "writer " << getpid() << " releases lock 2" << endl;
+            sem_post(semaphore2);
         }
-
-        /* shared memory detach */
-        shmdt(val);
-        shmctl(shm_id, IPC_RMID, 0);
-        /* cleanup semaphores */
-        sem_destroy(semaphore1);
-        sem_destroy(semaphore2);
     }
 
     while (pid == waitpid(-1, 0, NULL)) {
         if (errno == ECHILD)
             break;
     }
+
+    shm_unlink(name);
+    /* cleanup semaphores */
+    sem_destroy(semaphore1);
+    sem_destroy(semaphore2);
 
     exit(EXIT_SUCCESS);
 }
